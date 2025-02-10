af58a577a204b0b23e4c7a24dfd027ea0d756850
Junior <vilmarmjrr@gmail.com>
2025-02-10 19:36:09 -0300

    change logo src binding to computed

--- libs/web/core/layout/src/lib/logo/logo.component.ts
+++ libs/web/core/layout/src/lib/logo/logo.component.ts
@@ -1,24 +1,20 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { ThemeService } from '@web/shared/ui';
 
 @Component({
   selector: 'n-logo',
   imports: [CommonModule, NgOptimizedImage],
   template: `
-    <img
-      [ngSrc]="
-        theme() === 'dark' ? 'assets/img/logo-dark.svg' : 'assets/img/logo-light.svg'
-      "
-      height="28"
-      width="96"
-      alt="Notes App Logo"
-    />
+    <img [ngSrc]="src()" height="28" width="96" alt="Notes App Logo" />
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LogoComponent {
   private _themeService = inject(ThemeService);
 
-  protected theme = this._themeService.theme;
+  private theme = this._themeService.theme;
+  protected src = computed(() =>
+    this.theme() === 'dark' ? 'assets/img/logo-dark.svg' : 'assets/img/logo-light.svg',
+  );
 }

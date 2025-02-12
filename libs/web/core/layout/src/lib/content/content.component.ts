1123f48687908f7f556aa6628113d3113ad7c95a
Junior <vilmarmjrr@gmail.com>
2025-02-12 18:21:25 -0300

    fix content border radius

--- libs/web/core/layout/src/lib/content/content.component.ts
+++ libs/web/core/layout/src/lib/content/content.component.ts
@@ -6,7 +6,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [CommonModule],
   template: `
     <div
-      class="bg-base-white h-full rounded-lg px-4 py-5 sm:rounded-t-2xl sm:px-8 sm:py-6 dark:bg-neutral-950"
+      class="bg-base-white h-full rounded-t-lg px-4 py-5 sm:rounded-t-2xl sm:px-8 sm:py-6 dark:bg-neutral-950"
     >
       <ng-content />
     </div>

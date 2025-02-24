3c0cd48d244e8705ce7004c316e8d3a85102a14c
Junior <vilmarmjrr@gmail.com>
2025-02-24 18:03:54 -0300

    remove zone.js

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 import { apiInterceptor, withCredentialsInterceptor } from '@web/core/interceptors';
 import { environment } from '../environments/environment';
@@ -7,7 +10,7 @@ import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(appRoutes, withViewTransitions({ skipInitialTransition: true })),
     provideHttpClient(
       withInterceptors([apiInterceptor(environment.apiUrl), withCredentialsInterceptor]),

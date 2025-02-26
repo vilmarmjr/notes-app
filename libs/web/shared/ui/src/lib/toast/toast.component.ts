a94f7f79c41eb39ab2dd83bb443b817f2ee04cbd
Junior <vilmarmjrr@gmail.com>
2025-02-26 19:46:14 -0300

    change toast animation

--- libs/web/shared/ui/src/lib/toast/toast.component.ts
+++ libs/web/shared/ui/src/lib/toast/toast.component.ts
@@ -17,10 +17,9 @@ import { TOAST_CONFIG } from './toast.service';
   },
   animations: [
     trigger('slideInOut', [
-      state('void', style({ opacity: 0, transform: 'translateX(100%)' })),
-      state('*', style({ opacity: 1, transform: 'translateX(0)' })),
-      transition('void => *', [animate('200ms ease-in')]),
-      transition('* => void', [animate('200ms ease-out')]),
+      state('void', style({ opacity: 0, transform: 'translateY(200%)' })),
+      state('*', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition('void => *', [animate('300ms ease-in')]),
     ]),
   ],
   template: `

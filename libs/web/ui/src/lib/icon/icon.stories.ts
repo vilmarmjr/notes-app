39ef7c83b78a14cbf8f6e8ad618c418289df99d5
Junior <vilmarmjrr@gmail.com>
2025-03-23 21:03:57 -0300

    setup storybook with icon docs

---
+++ libs/web/ui/src/lib/icon/icon.stories.ts
@@ -0,0 +1,72 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { IconComponent, IconName, IconSize } from './icon.component';
+import { icons } from './icons';
+
+const sizeOptions: IconSize[] = [16, 20, 24, 28, 32, 36, 40, 44, 48, 56];
+
+const meta: Meta<IconComponent> = {
+  component: IconComponent,
+  title: 'Icon',
+};
+export default meta;
+type Story = StoryObj<IconComponent>;
+
+function buildStory(iconName: IconName): Story {
+  return {
+    args: {
+      size: 24,
+      name: iconName,
+    },
+    argTypes: {
+      size: {
+        options: sizeOptions,
+        control: 'select',
+        description: 'Size of the icon',
+        table: {
+          defaultValue: {
+            summary: '20',
+          },
+        },
+      },
+      name: {
+        options: Object.keys(icons),
+        control: 'select',
+        description: 'Name of the icon',
+      },
+    },
+    render: args => ({
+      props: args,
+      template: `<nt-icon [name]="name" [size]="size" />`,
+    }),
+  };
+}
+
+export const Archive = buildStory('archive');
+export const ArrowLeft = buildStory('arrowLeft');
+export const Checkmark = buildStory('checkmark');
+export const Crossmark = buildStory('crossmark');
+export const ChevronRight = buildStory('chevronRight');
+export const Clock = buildStory('clock');
+export const Cross = buildStory('cross');
+export const Delete = buildStory('delete');
+export const FontMonospace = buildStory('fontMonospace');
+export const FontSansSerif = buildStory('fontSansSerif');
+export const FontSerif = buildStory('fontSerif');
+export const Font = buildStory('font');
+export const Google = buildStory('google');
+export const HidePassword = buildStory('hidePassword');
+export const Home = buildStory('home');
+export const Info = buildStory('info');
+export const Lock = buildStory('lock');
+export const Logout = buildStory('logout');
+export const Menu = buildStory('menu');
+export const Moon = buildStory('moon');
+export const Plus = buildStory('plus');
+export const Restore = buildStory('restore');
+export const Search = buildStory('search');
+export const Settings = buildStory('settings');
+export const ShowPassword = buildStory('showPassword');
+export const Status = buildStory('status');
+export const Sun = buildStory('sun');
+export const SystemTheme = buildStory('systemTheme');
+export const Tag = buildStory('tag');

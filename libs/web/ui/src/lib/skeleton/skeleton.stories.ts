190fdb3da4e8e4aa185472cc2a4ab79473070099
Junior <vilmarmjrr@gmail.com>
2025-03-25 20:40:18 -0300

    add skeleton stories

---
+++ libs/web/ui/src/lib/skeleton/skeleton.stories.ts
@@ -0,0 +1,33 @@
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+import { SkeletonComponent } from './skeleton.component';
+import { SkeletonModule } from './skeleton.module';
+
+const meta: Meta<SkeletonComponent> = {
+  component: SkeletonComponent,
+  title: 'Skeleton',
+  decorators: [
+    moduleMetadata({
+      imports: [SkeletonModule],
+    }),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<SkeletonComponent>;
+
+export const Default: Story = {
+  render: () => ({
+    template: `
+    <nt-skeleton class="flex flex-col gap-5">
+      <nt-skeleton-item class="mt-6 h-4 w-2/3 md:w-1/4" />
+      <nt-skeleton-item class="h-4 w-28" />
+      <nt-skeleton-item class="h-4 w-2/3 md:w-1/4" />
+      <nt-skeleton-item class="h-4 w-1/3" />
+      <nt-skeleton-item class="h-4 w-28" />
+      <nt-skeleton-item class="h-4 w-2/3 md:w-1/4" />
+      <nt-skeleton-item class="h-4 w-1/3" />
+    </nt-skeleton>
+    `,
+  }),
+};

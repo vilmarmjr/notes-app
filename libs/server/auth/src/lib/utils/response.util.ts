9d36ad2bf5baef184e11fea43b7215c67aee3cf7
Junior <vilmarmjrr@gmail.com>
2025-04-08 19:17:08 -0300

    fix clear cookie

--- libs/server/auth/src/lib/utils/response.util.ts
+++ libs/server/auth/src/lib/utils/response.util.ts
@@ -1,14 +1,16 @@
-import { Response } from 'express';
+import { CookieOptions, Response } from 'express';
 import { ACCESS_TOKEN_KEY } from '../constants/token.constants';
 
+const cookieOptions: CookieOptions = {
+  httpOnly: true,
+  sameSite: 'none',
+  secure: true,
+};
+
 export function setTokenCookie(token: string, response: Response) {
-  response.cookie(ACCESS_TOKEN_KEY, token, {
-    httpOnly: true,
-    sameSite: 'none',
-    secure: true,
-  });
+  response.cookie(ACCESS_TOKEN_KEY, token, cookieOptions);
 }
 
 export function clearTokenCookie(response: Response) {
-  response.clearCookie(ACCESS_TOKEN_KEY);
+  response.clearCookie(ACCESS_TOKEN_KEY, cookieOptions);
 }
